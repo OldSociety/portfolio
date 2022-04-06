5d743413be211630260f21d7b6ff9aab4c898a54
OldSociety <headmistrust@outlook.com>
2022-04-05 20:48:43 -0700

    example

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>About
+      We don’t believe in the idea that you’re either born smart or not. We believe that every woman has the ability and power within her to become extraordinarily smart regardless of her circumstances, background, and past grades.
+
+SheRocks@College will equip you to succeed in college so that you can be proud of who your education helps you become.
+We’re here to serve the next generation of female leaders 
+
+We know the impact passing your courses will have on your college education. How it will impact your career, income, self confidence and the life you will live.
+
+ 
+Why? Because it did for me.
+
+By the time I was a junior in college I had failed out of 7 courses and my advisor told me I’d never make it in the medical field because I wasn’t smart enough.
+
+A year later I graduated college with a far from impressive 2.7 gpa, couldn’t apply to medical school and got rejected from an accelerated nursing program.
+
+I felt like a failure and like the dumbest girl in the world.
+
+But even then, the one thing I wouldn’t allow was for anyone to set limits to what I can and cannot do.
+    </div>
   )
 }
 
